0902dae4dd324177b20ec581213f1a9217fbb25c
Pathi_rao <lakshmipathi0000@gmail.com>
2021-09-06 12:38:45 +0200

    Update client.py

--- src/client.py
+++ src/client.py
@@ -9,14 +9,6 @@ from sklearn.metrics import log_loss
 
 import utils
 
-# dataset info
-"""
-    https://kdd.ics.uci.edu/databases/kddcup99/kddcup99.html
-    Objective: Build a network intrusion detector, a predictive model 
-    capable of distinguishing between ``bad'' connections, called intrusions or attacks, and ``good'' normal connections. This database contains a standard 
-    set of data to be audited, which includes a wide variety of intrusions simulated in a military network environment.
-
-    """
 
 X_train, X_test, y_train, y_test = dh.data_processor()
 
@@ -29,13 +21,6 @@ partition_id = np.random.choice(10)
 #print(X_train.shape, y_train.shape)
 
 
-""" 
-    Initialize the logistic regression model in the client. Let's have the client train for just a single iteration in each round by setting max_iter=1.
-    Also, don't forget to set warm_start=True, otherwise, the model's parameters get refreshed when we call .fit. We don't want to reset the global parameters 
-    sent by the server.
-
-    """
-
 # Create LogisticRegression Model
 model = LogisticRegression(
 penalty="l2",
@@ -43,30 +28,8 @@ max_iter=1, # local epoch
 warm_start=True, # prevent refreshing weights when fitting
 )
 
-"""
-    Next, we have to set the initial parameters of the model since the instance attributes used to save the model's parameters aren't created until .fit is called.
-    But the server might want to set them or request them before fitting as is usually the case in federated learning. So, we create the parameter attributes and zero-initialize
-    them using utils.set_initial_params(model).
-
-    """
 # Setting initial parameters, akin to model.compile for keras models
 utils.set_initial_params(model)
-
-"""
-    Now it is time to define the Flower client. The client is derived from the class fl.client.NumPyClient. It needs to define the following three methods:
-
-    get_parameters : Returns the current local model paramters. The utility function get_model_parameters does this for us.
-    
-    fit: Defines the steps to train the model on the locally held dataset. It also receives global model parameters and other configuration information from the server. 
-    We update the local model's parameters using the received global parameters using utils.set_model_params(model, parameters) and train it on the local dataset. 
-    This method also sends back the local model's parameters after training, the size of the training set and a dict communicating arbitrary values back to the server.
-
-    evaluate: This method is meant for evaluating the provided parameters using a locally held dataset. It returns the loss along with other details such as the size 
-    of the test set, accuracy, etc., back to the server. Here, we calculate the loss value of the model explicitly using sklearn.metrics.log_loss. This is done explicitly 
-    because there is no public attribute in LogisticRegression that saves the loss value like, for instance, a TensorFlow model's history. Make sure to use the proper
-    loss function corresponding to your model.
-
-    """
 
 
 class KDDClient(fl.client.NumPyClient):
